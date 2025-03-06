a2bfbf6d62b1ea19dce411aea78d354ca471449d
Localxxv <localgxv@gmail.com>
2025-03-06 19:17:45 +0100

    1 test

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+        int sum = a + b;
+
+        
+        System.out.println("Hello, its java");
+        System.out.println("Sum of " + a + " and " + b + " is: " + sum);
+    }
+}
